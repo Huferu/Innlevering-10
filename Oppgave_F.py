f968a179c582a70f5e09fd718c58522e6dc172ea
Huferu <123447191+Huferu@users.noreply.github.com>
2024-11-05 11:42:28 +0100

    Fikset Oppgave F
    Nå plotter den standardavvik

--- Oppgave_F.py
+++ Oppgave_F.py
@@ -23,6 +23,8 @@
 
 import math
 import matplotlib.pyplot as plt
+from Oppgave6d import LagListe
+from Oppgave_e import convert_to_datetime as Konverter_til_datatid
 
 #Øving 6 
 
@@ -40,10 +42,13 @@ def standardavvik(n:int, y:list, _y:float):
     sum = math.sqrt(sum/(n-1))
     return sum
 
+liste = LagListe('trykk_og_temperaturlogg_rune_time.csv.txt',5)
+tid = [int(tid) for tid in liste[1]]
+temperaturer = [float(temp.replace(',','.')) for temp in liste[4]]
 
-
-standardavvik_liste = standardavvik(gjennomsnitt(2, list, list))
-avstand = 30
+standardavvik_liste = standardavvik(len(temperaturer),temperaturer,gjennomsnitt(len(temperaturer), temperaturer))
+avstand = 100
 hvor_lang = 2 #en viss avstand idk
 
-plt.errorbar(yerr=standardavvik_liste, errorevery=avstand, capsize=hvor_lang)
+plt.errorbar(y=temperaturer,x=tid,yerr=standardavvik_liste, errorevery=avstand, capsize=hvor_lang)
+plt.show()
